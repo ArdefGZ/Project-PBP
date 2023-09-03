fa476066fb1bdea801df93d8d5674bfef56dc2fe
ArdefGZ <112787378+ArdefGZ@users.noreply.github.com>
2023-09-03 13:04:00 +0700

    Update script.js
    bisa rotate + popup start

--- Snake Game Kelompok 7/script.js
+++ Snake Game Kelompok 7/script.js
@@ -16,6 +16,7 @@ let velocityX = 0, velocityY = 0;
 let snakeBody = [];
 let setIntervalId;
 let score = 0;
+let gameStarted = false;
 
 // Mendapat Skor Tertinggi dari Penyimpanan Lokal
 let highScore = localStorage.getItem("high-score") || 0;
@@ -32,6 +33,16 @@ const handleGameOver = () => {
     const popupContainer = document.getElementById("popupContainer");
     popupContainer.style.display = "flex";
 }
+
+const startButton = document.getElementById("startButton");
+
+startButton.addEventListener("click", () => {
+  const startPopup = document.getElementById("startPopup");
+  startPopup.style.display = "none";
+  gameStarted = true;
+  // Start the game here (e.g., by calling your initGame function)
+});
+
 
 const changeDirection = e => {
     // Mengubah Arah Gerak Berdasarkan Tombol
@@ -54,44 +65,66 @@ const changeDirection = e => {
 controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
 
 const initGame = () => {
-    if(gameOver) return handleGameOver();
+    if (gameOver) return handleGameOver();
     let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
-
+  
     // Checking if the snake hit the food
-    if(snakeX === foodX && snakeY === foodY) {
-        updateFoodPosition();
-        snakeBody.push([foodY, foodX]); // Pushing food position to snake body array
-        score++; // increment score by 1
-        highScore = score >= highScore ? score : highScore;
-        localStorage.setItem("high-score", highScore);
-        scoreElement.innerText = `Score: ${score}`;
-        highScoreElement.innerText = `High Score: ${highScore}`;
+    if (snakeX === foodX && snakeY === foodY) {
+      updateFoodPosition();
+      snakeBody.push([foodY, foodX]); // Pushing food position to snake body array
+      score++; // increment score by 1
+      highScore = score >= highScore ? score : highScore;
+      localStorage.setItem("high-score", highScore);
+      scoreElement.innerText = `Score: ${score}`;
+      highScoreElement.innerText = `High Score: ${highScore}`;
     }
     // Updating the snake's head position based on the current velocity
     snakeX += velocityX;
     snakeY += velocityY;
-    
+  
+    // Determine the direction and apply the corresponding CSS class
+    let directionClass = "";
+    if (velocityX === -1) {
+      directionClass = "pacman-left";
+    } else if (velocityX === 1) {
+      directionClass = "pacman-right";
+    } else if (velocityY === -1) {
+      directionClass = "pacman-up";
+    } else if (velocityY === 1) {
+      directionClass = "pacman-down";
+    }
+  
     // Shifting forward the values of the elements in the snake body by one
     for (let i = snakeBody.length - 1; i > 0; i--) {
-        snakeBody[i] = snakeBody[i - 1];
+      snakeBody[i] = snakeBody[i - 1];
     }
     snakeBody[0] = [snakeX, snakeY]; // Setting first element of snake body to current snake position
-
+  
     // Checking if the snake's head is out of wall, if so setting gameOver to true
-    if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
-        return gameOver = true;
+    if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+      return (gameOver = true);
     }
-
+  
     for (let i = 0; i < snakeBody.length; i++) {
-        // Adding a div for each part of the snake's body
-        html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
-        // Checking if the snake head hit the body, if so set gameOver to true
-        if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
-            gameOver = true;
-        }
+      // Adding a div for each part of the snake's body with the direction class
+      html += `<div class="head ${directionClass}" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+      // Checking if the snake head hit the body, if so set gameOver to true
+      if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
+        gameOver = true;
+      }
     }
     playBoard.innerHTML = html;
-}
+};
+
+// Add this at the end of your JavaScript code
+window.addEventListener("load", () => {
+    const startPopup = document.getElementById("startPopup");
+    startPopup.style.display = "flex";
+    if (!gameStarted) {
+        startPopup.style.display = "flex";
+    }
+  });
+
 
 const closePopupButton = document.getElementById("closePopup");
 
@@ -99,6 +132,7 @@ closePopupButton.addEventListener("click", () => {
     const popupContainer = document.getElementById("popupContainer");
     popupContainer.style.display = "none";
     location.reload();
+    startPopup.style.display = "none";
 });
 
 updateFoodPosition();
