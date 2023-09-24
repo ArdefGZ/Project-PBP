a82dc4558f806e5e4ec37cd89b86514fe3732d70
rafifabr <100357100+rafifabr@users.noreply.github.com>
2023-09-24 22:49:41 +0700

    Update graf_buku.php

--- TugasMatkul/graf_buku.php
+++ TugasMatkul/graf_buku.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rekap Data Buku</title>
+    <title>Tugas PBP - Koneksi ke Basis Data</title>
     <!-- Include Chart.js -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="style/graf_buku_styles.css">
