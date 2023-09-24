b8f6158b4c6f9fd94f983850625ddbc5d61adab5
dhimzy <100180259+dhimzy@users.noreply.github.com>
2023-09-24 21:39:03 +0700

    add_book.php

--- TugasMatkul/add_book.php
+++ TugasMatkul/add_book.php
@@ -61,7 +61,7 @@ if (isset($_POST["submit"])){
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Tugas PBP - Koneksi ke Basis Data</title>
     <link rel="stylesheet" href="style/add_book_styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
