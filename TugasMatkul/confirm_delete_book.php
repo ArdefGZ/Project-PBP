4ab3d62476cd7e6f52eae798b6473bc5fcca30eb
rafifabr <100357100+rafifabr@users.noreply.github.com>
2023-09-24 22:33:18 +0700

    Update confirm_delete_book.php

--- TugasMatkul/confirm_delete_book.php
+++ TugasMatkul/confirm_delete_book.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tugas Praktikum 4</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/confirm_delete_book_styles.css">
 </head>
 
 <body> 
