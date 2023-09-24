580bc1de5190b2ec32b52ef7ef33b3dc96a415bd
dhimzy <100180259+dhimzy@users.noreply.github.com>
2023-09-24 20:59:02 +0700

    add_book.php

--- TugasMatkul/add_book.php
+++ TugasMatkul/add_book.php
@@ -62,6 +62,7 @@ if (isset($_POST["submit"])){
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
+    <link rel="stylesheet" href="style/add_book_styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
   <body>
