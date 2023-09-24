97fa52e5bed1edb3eb83a9428fe74a18729a02c9
dhimzy <100180259+dhimzy@users.noreply.github.com>
2023-09-24 23:30:24 +0700

    edit_book.php

--- TugasMatkul/edit_book.php
+++ TugasMatkul/edit_book.php
@@ -74,7 +74,7 @@ if (!isset($_POST["submit"])){
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Tugas PBP - Koneksi ke Basis Data</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="style/edit_book_styles.css">
   </head>
