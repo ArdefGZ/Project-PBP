b33cbca5a4270bfcf81c89257fed6f62ad9b2669
ArdefGZ <112787378+ArdefGZ@users.noreply.github.com>
2023-09-24 20:34:18 +0700

    Update show_cart.php

--- TugasMatkul/show_cart.php
+++ TugasMatkul/show_cart.php
@@ -52,14 +52,24 @@ if($id != ""){
                 $sum_qty = 0; 
                 //inisialisasi total price di shopping cart
                 $sum_price = 0; 
-                if (is_array($_SESSION['cart'])){
-                    foreach ($_SESSION['cart'] as $id => $quantity){
-                        $query = " SELECT * FROM books WHERE isbn='".$id."'";
-                        $result = $db->query($query);
-                        if (!$result){
-                            die("Could not query the database: <br>".$db->error."<br>Query: " .$query);
+                if (is_array($_SESSION['cart'])) {
+                    foreach ($_SESSION['cart'] as $id => $quantity) {
+                        // Use a prepared statement to prevent SQL injection
+                        $query = "SELECT * FROM books WHERE isbn = ?";
+                        $stmt = $db->prepare($query);
+                        $stmt->bind_param("s", $id);
+                        
+                        if (!$stmt->execute()) {
+                            die("Could not execute the query: " . $stmt->error);
                         }
-                        while ($row = $result->fetch_object()){
+                        
+                        $result = $stmt->get_result();
+                        
+                        if (!$result) {
+                            die("Could not get the result: " . $db->error);
+                        }
+                        
+                        while ($row = $result->fetch_object()) {
                             echo '<tr>';
                             echo '<td>'.$row->isbn.'</td>';
                             echo '<td>'.$row->author.'</td>';
@@ -67,17 +77,17 @@ if($id != ""){
                             echo '<td>'.$row->price.'</td>';
                             echo '<td>'.$quantity.'</td>';
                             $price = $row->price;
-                            echo '<td>'.$price*$quantity.'</td>';
+                            echo '<td>'.$price * $quantity.'</td>';
                             $sum_price = $sum_price + ($row->price * $quantity);
                             $sum_qty = $sum_qty + $quantity;
                         }
+                        
+                        $result->free();
+                        $stmt->close();
                     }
-                    echo '<tr><td></td><td></td><td></td><td></td><td>'.$sum_qty.'</td><td>'.$sum_price.'</td></tr>';
-                    $result->free();
-                    $db->close();
-                } else{
-                    echo '<tr><td colspan="6" align="center">There is no item in shopping cart</td></tr>';
-                } 
+                } else {
+                    echo '<tr><td colspan="6" align="center">There is no item in the shopping cart</td></tr>';
+                }
                 ?>
             </table>
             Total items = <?php echo $sum_qty; ?><br><br>
